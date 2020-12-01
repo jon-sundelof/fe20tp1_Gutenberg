bc3a70747f0df2b77132f86e5ff4185ec4702392
jonsundelof <jon.sundelof@student.kyh.se>
2020-12-01 15:30:13 +0100

    Sparfunktion och E.target på min branch

--- main.js
+++ main.js
@@ -9,7 +9,9 @@ let toolbarOptions = [
     [{ 'header': [1, 2, 3, false] }],   
 ];
 
-
+//Generear string för datum  (Snodde från Onur's kod <3 )
+const datum = new Date();
+const date = datum.getHours() + ":" + ((datum.getMinutes() < 10 ? '0' : '') + datum.getMinutes()) + ' / ' + datum.getFullYear() + '-' + (datum.getMonth() + 1) + '-' + ((datum.getDate() < 10 ? '0' : '') + datum.getDate());
 
 //const printscreen = document.querySelector('.printscreen')
 const notePreview = document.querySelector(".preview-notes");
@@ -21,7 +23,7 @@ const note = document.querySelector("#editor")
 const btnAdd = document.querySelector(".add");
 const btnPrint = document.querySelector(".print");
 
-const titleInput = document.querySelector("#title-input")
+const titleInput = document.querySelector("#title-input");
 
 /************************/
 //******** QUILL ********/
@@ -50,7 +52,26 @@ let savedNotes = [];
         savedNotes = [];
     } else {
         savedNotes = JSON.parse(localStorage.getItem('savedNotes'));
+
+        for (let i = 0; i <savedNotes.length; i++) {
+            const preDiv = document.createElement("div");
+            preDiv.innerHTML = `<h3>${savedNotes[i].title}</h3><p>${savedNotes[i].text}</p><p>${savedNotes[i].date}</p>`;
+            preDiv.setAttribute('class', 'preDiv');
+            preDiv.setAttribute('id', savedNotes[i].id);
+            notePreview.append(preDiv);
+          }
     }
+
+/* function checkForInput(){ 
+if (titleInput.value == "" || titleInput.value == "New Note"){
+
+    return "New Note";
+
+} else {
+    return titleInput.value;
+    }
+} */
+
 
 //*Hämtar data från local storage
 load = () => {
@@ -76,16 +97,139 @@ class Note {
 
 }
 
-let newNote = savedNotes[0]
+/************************/
+//**** Target.Event function för att kunna "target" sina anteckningar *****/
+/************************/
+
+const previewDiv = document.querySelectorAll('.preDiv');
+let pressedPreview = false;
+
+previewDiv.forEach(item => {
+    item.addEventListener('click', event => {
+      //handle click
+    let thisDivId = event.target.closest('.preDiv').id;
+    console.log(thisDivId);
+    for (let i = 0; i < savedNotes.length; i++){
+        if (thisDivId == savedNotes[i].id.toString()){
+            
+            editor.setText(savedNotes[i].text);
+            titleInput.value = savedNotes[i].title;
+
+            currentNoteId = thisDivId;
+            pressedPreview = true;
+
+        }
+        }
+     });
+  });
+
+
+
+
+
+
+
+//kommenterade ut då jag ej fatta vad den är till för? :3
+/* let newNote = savedNotes[0] */
+
+//Variabel för vilket id den aktiva noten har 
+let currentNoteId;
+let length = savedNotes.length;
+let currentNote;
 
 setInterval( () => {
-    
-    //Hitta innehållet, om id ej finns, spara i local storage
+
+    if(pressedPreview == false){
+
+    if (length <= 0 || savedNotes[savedNotes.length - 1].id !== currentNoteId){
+        textContent = editor.getText();
+        /* checkForInput() */
+        let newNote = new Note (titleInput.value, date, textContent, false);
+        savedNotes.push(newNote);
+        localStorage.setItem("savedNotes", JSON.stringify(savedNotes));
+
+
+        const preDiv = document.createElement("div");
+        preDiv.innerHTML = `<h3>${savedNotes[savedNotes.length - 1].title}</h3><p>${savedNotes[savedNotes.length - 1].text}</p><p>${savedNotes[savedNotes.length - 1].date}</p>`;
+        preDiv.setAttribute('class', 'preDiv');
+        preDiv.setAttribute('id', savedNotes[savedNotes.length - 1].id);
+        notePreview.append(preDiv);
+
+        currentNote = savedNotes[savedNotes.length - 1];
+
+     
+    } else {
+
+
+    for (let i = 0; i < savedNotes.length; i++){
+        if (currentNoteId == savedNotes[i].id.toString()){
+            textContent = editor.getText()
+            savedNotes[i].title = titleInput.value;
+            savedNotes[i].date = date;
+            savedNotes[i].text = textContent;
+            savedNotes[i].star = false;
+
+        }
+    }
+
+   /*  savedNotes.push(newNote); */
+    localStorage.setItem("savedNotes", JSON.stringify(savedNotes));
+
+    let nodes = document.querySelectorAll(".preDiv");
+    for(var i = 0; i < nodes.length; i++){
+    nodes[i].parentNode.removeChild(nodes[i]);
+    }
+
+    for (let i = 0; i < savedNotes.length; i++) {
+        const preDiv = document.createElement("div");
+        preDiv.innerHTML = `<h3>${savedNotes[i].title}</h3><p>${savedNotes[i].text}</p><p>${savedNotes[i].date}</p>`;
+        preDiv.setAttribute('class', 'preDiv');
+        preDiv.setAttribute('id', savedNotes[i].id);
+        notePreview.append(preDiv);
+      }
+ 
+}
+
+    if(pressedPreview == false) {
+        currentNoteId = savedNotes[savedNotes.length -1].id;
+    }
+  } else {
+      
+    for (let i = 0; i < savedNotes.length; i++){
+        if (currentNoteId == savedNotes[i].id.toString()){
+            textContent = editor.getText()
+            savedNotes[i].title = titleInput.value;
+            savedNotes[i].date = date;
+            savedNotes[i].text = textContent;
+            savedNotes[i].star = false;
+
+        }
+    }
+
+   /*  savedNotes.push(newNote); */
+    localStorage.setItem("savedNotes", JSON.stringify(savedNotes));
+
+    let nodes = document.querySelectorAll(".preDiv");
+    for(var i = 0; i < nodes.length; i++){
+    nodes[i].parentNode.removeChild(nodes[i]);
+    }
+
+    for (let i = 0; i < savedNotes.length; i++) {
+        const preDiv = document.createElement("div");
+        preDiv.innerHTML = `<h3>${savedNotes[i].title}</h3><p>${savedNotes[i].text}</p><p>${savedNotes[i].date}</p>`;
+        preDiv.setAttribute('class', 'preDiv');
+        preDiv.setAttribute('id', savedNotes[i].id);
+        notePreview.append(preDiv);
+      }
+  }
+   
+length = savedNotes.length;
+
+  //Hitta innehållet, om id ej finns, spara i local storage
     //* HÄR ONUR
-    textContent = editor.getText()
-    
-    
-    if(savedNotes[0].id == newNote.id){
+
+//Kommenterade ut din karl. :) Hoppas de inte är några konstigheter
+ /*    if(savedNotes[0].id == newNote.id){
         console.log("Array är inte tom")
 
         newNote.text = textContent;
@@ -96,8 +240,10 @@ setInterval( () => {
     } else {
         console.log("Array är tom")
         savedNotes.push(newNote)
-    }
-}, 20000);
+    } */
+ 
+}, 3000);
+
 
 //*En function där våran note skapas
 function createNote() {
